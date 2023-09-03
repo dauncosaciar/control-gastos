4c7ce4c82018980ef2cb87781b931417edbf5af4
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-09-03 20:04:57 -0300

    Add currency format to amounts and button to new spent

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,11 @@
 const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("es-AR", {
+      style: "currency",
+      currency: "ARS"
+    });
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -7,7 +14,16 @@ const ControlPresupuesto = ({ presupuesto }) => {
 
       <div className="contenido-presupuesto">
         <p>
-          <span>Presupuesto: </span> ${presupuesto}
+          <span>Presupuesto: </span>
+          {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible: </span>
+          {formatearCantidad(0)}
+        </p>
+        <p>
+          <span>Gastado: </span>
+          {formatearCantidad(0)}
         </p>
       </div>
     </div>
