e6e99c490b255bf17ee3285c4898877a47934a43
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2023-09-03 19:14:01 -0300

    Working on budget control component

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto, setIsValidPresupuesto }) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto válido");
+      return;
+    }
+
+    setMensaje("");
+    setIsValidPresupuesto(true);
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form className="formulario" onSubmit={handlePresupuesto}>
         <div className="campo">
           <label>Definir Presupuesto</label>
-          <input className="nuevo-presupuesto" type="text" placeholder="Añade tu presupuesto" />
+          <input
+            className="nuevo-presupuesto"
+            type="number"
+            placeholder="Añade tu presupuesto"
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
+          />
         </div>
 
         <input type="submit" value="Añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
       </form>
     </div>
   );
