4c78042b29962e0762f9c358bfa664efbd9cdaef
Daniel Alejandro Uncos Aciar <dev3.sitenso@tensolite.com>
2023-09-14 16:50:07 -0300

    Add localstorage to app

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import { generarId } from "./helpers";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem("gastos") ? JSON.parse(localStorage.getItem("gastos")) : []
+  );
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(Number(localStorage.getItem("presupuesto")) ?? 0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal] = useState(false);
@@ -25,6 +27,22 @@ function App() {
       }, 500);
     }
   }, [gastoEditar]);
+
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  useEffect(() => {
+    const presupuestoLocalStorage = Number(localStorage.getItem("presupuesto")) ?? 0;
+
+    if (presupuestoLocalStorage > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, []);
 
   const handleNuevoGasto = () => {
     setModal(true);
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
@@ -32,7 +32,15 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <CircularProgressbar value={porcentaje} />
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: "#3b82f6",
+            trailColor: "#f5f5f5",
+            textColor: "#3b82f6"
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
+        />
       </div>
 
       <div className="contenido-presupuesto">
